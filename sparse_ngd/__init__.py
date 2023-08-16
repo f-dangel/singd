d6275359db911c1c9ed42d5342fe786c728478ba
Felix Dangel <felix.dangel@vectorinstitute.ai>
2023-08-16 08:54:37 -0400

    [DEL] Remove dummy tests from library template

--- sparse_ngd/__init__.py
+++ sparse_ngd/__init__.py
@@ -1,10 +1,1 @@
 """Sparse inverse-free natural gradient descent."""
-
-
-def hello(name):
-    """Say hello to a name.
-
-    Args:
-        name (str): Name to say hello to.
-    """
-    print(f"Hello, {name}")
