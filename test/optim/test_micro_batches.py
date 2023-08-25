d3e4e32eecb15926bfc1874e861061a5c3a6a940
Felix Dangel <felix.dangel@vectorinstitute.ai>
2023-08-25 15:57:38 -0400

    [REF] Improve names

--- test/optim/test_micro_batches.py
+++ test/optim/test_micro_batches.py
@@ -86,36 +86,32 @@ def test_micro_batches():
 
         # compare K, C, m_K, m_C
         assert len(optim_mini.modules) == len(optim_micro.modules)
-        for m_mini, m_ours in zip(optim_mini.modules, optim_micro.modules):
+        for m_mini, m_micro in zip(optim_mini.modules, optim_micro.modules):
             K_mini = optim_mini.Ks[m_mini].to_dense()
-            K_ours = optim_micro.Ks[m_ours].to_dense()
-            assert allclose(K_mini, K_ours, **tolerances)
+            K_micro = optim_micro.Ks[m_micro].to_dense()
+            assert allclose(K_mini, K_micro, **tolerances)
 
             m_K_mini = optim_mini.m_Ks[m_mini].to_dense()
-            m_K_ours = optim_micro.m_Ks[m_ours].to_dense()
-            assert allclose(m_K_mini, m_K_ours, **tolerances)
+            m_K_micro = optim_micro.m_Ks[m_micro].to_dense()
+            assert allclose(m_K_mini, m_K_micro, **tolerances)
 
             C_mini = optim_mini.Cs[m_mini].to_dense()
-            C_ours = optim_micro.Cs[m_ours].to_dense()
-            assert allclose(C_mini, C_ours, **tolerances)
+            C_micro = optim_micro.Cs[m_micro].to_dense()
+            assert allclose(C_mini, C_micro, **tolerances)
 
             m_C_mini = optim_mini.m_Cs[m_mini].to_dense()
-            m_C_ours = optim_micro.m_Cs[m_ours].to_dense()
-            assert allclose(m_C_mini, m_C_ours, **tolerances)
+            m_C_micro = optim_micro.m_Cs[m_micro].to_dense()
+            assert allclose(m_C_mini, m_C_micro, **tolerances)
 
         # compare momentum buffers
-        for p_mini, p_ours in zip(model_mini.parameters(), model_micro.parameters()):
+        for p_mini, p_micro in zip(model_mini.parameters(), model_micro.parameters()):
             mom_mini = optim_mini.state[p_mini]["momentum_buffer"]
-            mom_ours = optim_micro.state[p_ours]["momentum_buffer"]
-            assert allclose(mom_mini, mom_ours, **tolerances)
+            mom_micro = optim_micro.state[p_micro]["momentum_buffer"]
+            assert allclose(mom_mini, mom_micro, **tolerances)
 
         # compare model parameters
-        for p_mini, p_ours in zip(model_mini.parameters(), model_micro.parameters()):
-            assert allclose(p_mini, p_ours, **tolerances)
+        for p_mini, p_micro in zip(model_mini.parameters(), model_micro.parameters()):
+            assert allclose(p_mini, p_micro, **tolerances)
 
         if batch_idx >= MAX_STEPS:
             break
-
-
-if __name__ == "__main__":
-    test_micro_batches()
