e8c305a06cab292cfa0a1bf79362ae67937e6473
Felix Dangel <felix.dangel@vectorinstitute.ai>
2023-08-30 19:15:38 -0400

    [ADD] Test `TriuBottomRightDiagonalMatrix`

--- sparse_ngd/structures/triubottomrightdiag.py
+++ sparse_ngd/structures/triubottomrightdiag.py
@@ -2,8 +2,7 @@
 
 from __future__ import annotations
 
-import torch
-from torch import Tensor
+from torch import Tensor, arange, zeros
 
 from sparse_ngd.structures.base import StructuredMatrix
 
@@ -11,8 +10,15 @@ from sparse_ngd.structures.base import StructuredMatrix
 class TriuBottomRightDiagonalMatrix(StructuredMatrix):
     """Sparse upper-triangular matrix with bottom right diagonal entries.
 
-    [ r1 r2 ]
-    [ 0  D  ]
+    ``
+    [[r1, r2],
+    [[0,  D]]
+    ``
+
+    where
+    - ``r1`` is a scalar,
+    - ``r2`` is a row vector, and
+    - ``D`` is a diagonal matrix,
     """
 
     # TODO After the below basic functions are implemented, we can tackle the
@@ -23,8 +29,8 @@ class TriuBottomRightDiagonalMatrix(StructuredMatrix):
         """Store the matrix internally.
 
         Args:
-            diag: the diagonal elements of the matrix
-            row: the first row of the matrix
+            diag: The diagonal elements of the matrix (``diag(D)``).
+            row: the first row of the matrix (concatenation of ``r1`` and ``r2``).
         """
         assert diag.size(0) + 1 == row.size(0)
 
@@ -54,9 +60,9 @@ class TriuBottomRightDiagonalMatrix(StructuredMatrix):
             The represented matrix as PyTorch tensor.
         """
         dim = self._mat_row.size(0)
-        mat = torch.zeros((dim, dim))
+        mat = zeros((dim, dim), dtype=self._mat_row.dtype, device=self._mat_row.device)
 
-        k = torch.tensor(range(dim - 1)) + 1
+        k = arange(1, dim)
         mat[k, k] = self._mat_diag
         mat[0, :] = self._mat_row
 
---
+++ test/structures/test_triubottomrightdiag.py
@@ -0,0 +1,32 @@
+"""Test ``sparse_ngd.structures.triubottomrightdiag``."""
+
+from test.structures.utils import _TestStructuredMatrix
+
+from torch import Tensor, zeros_like
+
+from sparse_ngd.structures.triubottomrightdiag import TriuBottomRightDiagonalMatrix
+
+
+class TestTriuBottomRightDiagonalMatrix(_TestStructuredMatrix):
+    """Test suite for ``TriuBottomRightDiagonalMatrix`` class."""
+
+    STRUCTURED_MATRIX_CLS = TriuBottomRightDiagonalMatrix
+
+    def project(self, sym_mat: Tensor) -> Tensor:
+        """Project a symmetric matrix onto a triu matrix w/ bottom right diagonal.
+
+        Args:
+            sym_mat: A symmetric matrix.
+
+        Returns:
+            An upper-triangular matrix with bottom right diagonal.
+        """
+        tril_bottom_right_diag = zeros_like(sym_mat)
+
+        # first row (except for first entry)
+        tril_bottom_right_diag[0, 1:] = sym_mat[0, 1:] + sym_mat[1:, 0]
+        # diagonal
+        for i in range(sym_mat.shape[0]):
+            tril_bottom_right_diag[i, i] = sym_mat[i, i]
+
+        return tril_bottom_right_diag
