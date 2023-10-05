f57e512f2f9edbcd73936ed45082ee83ba202b5a
Felix Dangel <felix.dangel@vectorinstitute.ai>
2023-10-05 15:51:50 +0200

    [DEL] Clean up after pruning commits

--- sparse_ngd/experimental/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Experimental functionality of ``sparse_ngd``.
-
-NOTE: USE WITH CAUTION!
-
-We do not provide any guarantees for this code.
-It is un-tested, un-documented, un-linted, and might be removed at any time.
-"""
