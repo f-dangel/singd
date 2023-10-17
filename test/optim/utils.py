517739b1417a74f900e62a4532d28cddc4bb020c
runame <re393@cam.ac.uk>
2023-10-17 22:08:22 +0200

    Add naive Jacobian util for KFAC test

--- test/optim/utils.py
+++ test/optim/utils.py
@@ -3,6 +3,8 @@
 from typing import Tuple
 
 import torch
+from torch import Tensor
+from torch.nn import Module
 
 from singd.optim.optimizer import SINGD
 from singd.structures.base import StructuredMatrix
@@ -77,3 +79,34 @@ def verify_dtype(mat: StructuredMatrix, dtype: torch.dtype):
     dtypes = [tensor.dtype for tensor in tensors_to_check]
     if any(d != dtype for d in dtypes):
         raise RuntimeError(f"Expected dtype {dtype}, got {dtypes}.")
+
+
+def grad(model: Module) -> Tensor:
+    return torch.cat([p.grad.data.flatten() for p in model.parameters()]).detach()
+
+
+def jacobians_naive(model: Module, data: Tensor, setting: str) -> Tuple[Tensor, Tensor]:
+    model.zero_grad()
+    try:
+        f: Tensor = model(data, setting)
+    except TypeError:
+        f: Tensor = model(data)
+    Jacs = list()
+    for i in range(f.shape[0]):
+        if len(f.shape) > 1:
+            jacs = list()
+            for j in range(f.shape[1]):
+                rg = i != (f.shape[0] - 1) or j != (f.shape[1] - 1)
+                torch.autograd.backward(f[i, j], torch.tensor(1.0), retain_graph=rg)
+                Jij = grad(model)
+                jacs.append(Jij)
+                model.zero_grad()
+            jacs = torch.stack(jacs).t()
+        else:
+            rg = i != (f.shape[0] - 1)
+            f[i].backward(retain_graph=rg)
+            jacs = grad(model)
+            model.zero_grad()
+        Jacs.append(jacs)
+    Jacs = torch.stack(Jacs).transpose(1, 2)
+    return Jacs.detach(), f.detach()
