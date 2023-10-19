40498be3d2d40cb08ecff43a04f99bab3cdabb10
Felix Dangel <48687646+f-dangel@users.noreply.github.com>
2023-10-19 18:18:40 -0400

    [ADD] Recursive template for bottom-left-matrices (#28)
    Part 2/2 of https://github.com/yorkerlin/Sparse-NGD/issues/5.
    
    This PR is the equivalent of #27, but adds a template for
    ```bash
    [ [A, 0],
      [B, C] ]
    ```
    where `A`, `C` are structured matrices that can be recursive, and `B` is rectangular dense.
    
    - Replaces the existing `Tril{TopLeft,BottomRight}DiagonalMatrix` implementations with inheritance from the recursive template. So we don't need more tests for now. 
    
    
    * [REF] Make `_tensors_to_sync` abstract and strictly return tuples
    
    This simplifies the implementation for recursive matrices and moves
    the `NotImplementedError` error for data parallel support closer to
    the method that needs to be implemented.
    
    * [REF] Implement `{TriuTopLeft,TriuBottomRight}DiagonalMatrix` recursively
    
    * [DEL] Redundant documentation
    
    * [REF] Extract function that checks for square-matrix input
    
    * [REF] Extract function to compute the boundary index
    
    * [FIX] Documentation formatting and correctness
    
    * [FIX] Typo in error message
    
    * [REF] Use `block_diag`
    
    * [REF] Extract `_get_boundary`
    
    * [REF] Implement `Tril{BottomRight,TopLeft}DiagonalMatrix` via recursion

--- singd/structures/trilbottomrightdiag.py
+++ singd/structures/trilbottomrightdiag.py
@@ -1,15 +1,11 @@
 """Toeplitz matrix implemented in the ``StructuredMatrix`` interface."""
 
-from __future__ import annotations
-
-from typing import Tuple
-
-from torch import Tensor, arange, zeros
-
-from singd.structures.base import StructuredMatrix
+from singd.structures.dense import DenseMatrix
+from singd.structures.diagonal import DiagonalMatrix
+from singd.structures.recursive import RecursiveBottomLeftMatrixTemplate
 
 
-class TrilBottomRightDiagonalMatrix(StructuredMatrix):
+class TrilBottomRightDiagonalMatrix(RecursiveBottomLeftMatrixTemplate):
     """Sparse lower-triangular matrix with bottom right diagonal.
 
     ``
@@ -23,59 +19,6 @@ class TrilBottomRightDiagonalMatrix(StructuredMatrix):
     - ``D`` is a diagonal matrix.
     """
 
-    # TODO After the below basic functions are implemented, we can tackle the
-    # specialized ones, then eventually remove this line
-    WARN_NAIVE: bool = False  # Fall-back to naive base class implementations OK
-
-    def __init__(self, diag: Tensor, col: Tensor) -> None:
-        """Store the matrix internally.
-
-        Args:
-            diag: The diagonal elements of the matrix (``diag(D)``).
-            col: The first column of the matrix (concatenation of ``c1`` and ``c2``).
-        """
-        assert diag.size(0) + 1 == col.size(0)
-
-        self._mat_col = col
-        self._mat_diag = diag
-
-    @property
-    def _tensors_to_sync(self) -> Tuple[Tensor, Tensor]:
-        """Tensors that need to be synchronized across devices.
-
-        This is used to support distributed data parallel training.
-
-        Returns:
-            A tuple of tensors that need to be synchronized across devices.
-        """
-        return (self._mat_col, self._mat_diag)
-
-    @classmethod
-    def from_dense(cls, mat: Tensor) -> TrilBottomRightDiagonalMatrix:
-        """Construct from a PyTorch tensor.
-
-        Args:
-            mat: A dense and symmetric square matrix which will be approximated by a
-                ``TrilBottomRightDiagonalMatrix``.
-
-        Returns:
-            ``TrilBottomRightDiagonalMatrix`` approximating the passed matrix.
-        """
-        diag = mat.diag()
-        col = mat[:, 0] + mat[0, :]
-        col[0] = diag[0]
-        return cls(diag[1:], col)
-
-    def to_dense(self) -> Tensor:
-        """Convert into dense PyTorch tensor.
-
-        Returns:
-            The represented matrix as PyTorch tensor.
-        """
-        dim = self._mat_col.size(0)
-        mat = zeros((dim, dim), dtype=self._mat_col.dtype, device=self._mat_col.device)
-        k = arange(1, dim)
-        mat[k, k] = self._mat_diag
-        mat[:, 0] = self._mat_col
-
-        return mat
+    MAX_DIMS = (1, float("inf"))
+    CLS_A = DenseMatrix
+    CLS_C = DiagonalMatrix
